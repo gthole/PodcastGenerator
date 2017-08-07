0941d1b7e054dbff8783cdc0f97b82d4deddbc23
Greg Thole <gthole@gmail.com>
2017-08-07 11:41:24 -0400

    Add page google analytics

--- core/admin/loadjavascripts.php
+++ core/admin/loadjavascripts.php
@@ -4,39 +4,49 @@
 #
 # Created by Alberto Betella
 # http://podcastgen.sourceforge.net
-# 
+#
 # This is Free Software released under the GNU/GPL License.
 ############################################################
 
+include("config.php");
 
 $loadjavascripts = '<script type="text/javascript" src="components/js/admin.js"></script>
 
 <script type="text/javascript" src="components/js/jquery.js"></script>
 ';
 
+if (isset($podcast_ga_id)) {
+    $loadjavascripts .= "<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', '".$podcast_ga_id."', 'auto');
+      ga('send', 'pageview');
+    </script>";
+}
+
+if (isset($_GET["do"])) {
 
 
-if (isset($_GET["do"])) { 
+    if ($_GET["do"]=="upload" OR $_GET["do"]=="freebox" OR $_GET["do"]=="edit" OR $_GET["do"]=="categories") {
 
 
-	if ($_GET["do"]=="upload" OR $_GET["do"]=="freebox" OR $_GET["do"]=="edit" OR $_GET["do"]=="categories") {
-
-	
-	//DELETE FADING IN CONFIRMATION (e.g. are you sure u want to delete?)	
+    //DELETE FADING IN CONFIRMATION (e.g. are you sure u want to delete?)
 $loadjavascripts .='
-	<script type="text/javascript">
+    <script type="text/javascript">
 $(document).ready(
     function() {
         $("#confirmdelete").click(function() {
             $("#confirmation").fadeToggle();
         });
     });
-</script>';	
+</script>';
 
 
 // DELETE categories confirmation
-	$loadjavascripts .='
-	<script type="text/javascript">
+    $loadjavascripts .='
+    <script type="text/javascript">
 $(document).ready(
     function() {
         $(\'[id^="confirmdelete-"]\').click(function() {
@@ -54,33 +64,33 @@ $(document).ready(
 // extended_valid_elements and custom_elements allows to insert new html elements (e.g. Google custom search engine)
 
 //NB in future enable "code" from toolbar below just for freebox
-		$loadjavascripts .='
-		<script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>
-		<script>
-		tinymce.init({
-		
-			selector:"#long_description",
-			extended_valid_elements : "gcse:search",
-			custom_elements : "gcse:search,~gcse:search",
-			entity_encoding : "raw",
-			width: 400,
-			height: 200,
-			menubar: false,
-			statusbar: false,
-			plugins: [
-					 "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-					 "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-					 "save table contextmenu directionality emoticons template paste textcolor"
-			],
-		   toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image | forecolor | code", 
+        $loadjavascripts .='
+        <script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>
+        <script>
+        tinymce.init({
 
-		});
-		</script>
-		';
+            selector:"#long_description",
+            extended_valid_elements : "gcse:search",
+            custom_elements : "gcse:search,~gcse:search",
+            entity_encoding : "raw",
+            width: 400,
+            height: 200,
+            menubar: false,
+            statusbar: false,
+            plugins: [
+                     "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+                     "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+                     "save table contextmenu directionality emoticons template paste textcolor"
+            ],
+           toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image | forecolor | code",
 
-		}
-	
-	}
+        });
+        </script>
+        ';
+
+        }
+
+    }
 
 
 
